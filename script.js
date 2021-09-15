ecbb9f5a003d5139bfcc16459f33e4a9606e3982
Chloe Nott <chloe@nott.email>
2021-09-14 18:41:41 -0700

    Adding js questions and bio

---
+++ script.js
@@ -0,0 +1,55 @@
+// What is your name?
+let userName = prompt("What is your name?")
+alert("Welcome " + userName + "!")
+
+// Generic alerts to yes/no answer questions.
+let invalid = "Invalid response.";
+let correct = "You were correct!"
+let incorrect = "That is incorrect. It's probably the other answer."
+
+// Deliberating not using a function, loop, nor map for the intent of this assignment.
+let response = prompt('Are cats my favorite kind of pet?').toLowerCase();
+if (response == 'yes' || response == 'y') {
+    console.log(correct);
+} else if (response == 'no' || response == 'n') {
+    console.log(incorrect);
+} else {
+    console.log(invalid);
+}
+
+response = prompt('Is watermelon not my favorite food?').toLowerCase();
+if (response == 'yes' || response == 'y') {
+    console.log(incorrect);
+} else if (response == 'no' || response == 'n') {
+    console.log(correct);
+} else {
+    console.log(invalid);
+}
+
+response = prompt('Is my favorite color blue?').toLowerCase();
+if (response == 'yes' || response == 'y') {
+    console.log(incorrect);
+} else if (response == 'no' || response == 'n') {
+    console.log(correct);
+} else {
+    console.log(invalid);
+}
+
+response = prompt('Do I enjoy listening to music?').toLowerCase();
+if (response == 'yes' || response == 'y') {
+    console.log(correct);
+} else if (response == 'no' || response == 'n') {
+    console.log(incorrect);
+} else {
+    console.log(invalid);
+}
+
+
+response = prompt('Is Gundam Unicorn my favorite anime?').toLowerCase();
+if (response == 'yes' || response == 'y') {
+    console.log(correct + " Thank you for playing this guessing game, " + userName + "!");
+} else if (response == 'no' || response == 'n') {
+    console.log(incorrect + " Thank you for playing this guessing game, " + userName + "!");
+} else {
+    console.log(invalid + " Thank you for playing this guessing game, " + userName + "!");
+}
