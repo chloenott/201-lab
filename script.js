645033eb881fe0ac75fd63e0f0dfa29d9cc2772c
Chloe Nott <chloe@nott.email>
2021-09-14 18:53:00 -0700

    Adding string validation to rest of questions."

--- script.js
+++ script.js
@@ -10,6 +10,7 @@ let incorrect = "That is incorrect. It's probably the other answer."
 // Deliberating not using a function, loop, nor map for the intent of this assignment.
 let response = prompt('Are cats my favorite kind of pet?')
 if (typeof(response) == 'string') {
+    response = response.toLowerCase();
     if (response == 'yes' || response == 'y') {
         console.log(correct);
     } else if (response == 'no' || response == 'n') {
@@ -21,39 +22,59 @@ if (typeof(response) == 'string') {
     console.log(invalid);
 }
 
-response = prompt('Is watermelon not my favorite food?').toLowerCase();
-if (response == 'yes' || response == 'y') {
-    console.log(incorrect);
-} else if (response == 'no' || response == 'n') {
-    console.log(correct);
+response = prompt('Is watermelon not my favorite food?')
+if (typeof(response) == 'string') {
+    response = response.toLowerCase();
+    if (response == 'yes' || response == 'y') {
+        console.log(incorrect);
+    } else if (response == 'no' || response == 'n') {
+        console.log(correct);
+    } else {
+        console.log(invalid);
+    }
 } else {
     console.log(invalid);
 }
 
-response = prompt('Is my favorite color blue?').toLowerCase();
-if (response == 'yes' || response == 'y') {
-    console.log(incorrect);
-} else if (response == 'no' || response == 'n') {
-    console.log(correct);
+response = prompt('Is my favorite color blue?')
+if (typeof(response) == 'string') {
+    response = response.toLowerCase();
+    if (response == 'yes' || response == 'y') {
+        console.log(incorrect);
+    } else if (response == 'no' || response == 'n') {
+        console.log(correct);
+    } else {
+        console.log(invalid);
+    }
 } else {
     console.log(invalid);
 }
 
-response = prompt('Do I enjoy listening to music?').toLowerCase();
-if (response == 'yes' || response == 'y') {
-    console.log(correct);
-} else if (response == 'no' || response == 'n') {
-    console.log(incorrect);
+response = prompt('Do I enjoy listening to music?')
+if (typeof(response) == 'string') {
+    response = response.toLowerCase();
+    if (response == 'yes' || response == 'y') {
+        console.log(correct);
+    } else if (response == 'no' || response == 'n') {
+        console.log(incorrect);
+    } else {
+        console.log(invalid);
+    }
 } else {
     console.log(invalid);
 }
 
 
-response = prompt('Is Gundam Unicorn my favorite anime?').toLowerCase();
-if (response == 'yes' || response == 'y') {
-    console.log(correct + " Thank you for playing this guessing game, " + userName + "!");
-} else if (response == 'no' || response == 'n') {
-    console.log(incorrect + " Thank you for playing this guessing game, " + userName + "!");
+response = prompt('Is Gundam Unicorn my favorite anime?')
+if (typeof(response) == 'string') {
+    response = response.toLowerCase();
+    if (response == 'yes' || response == 'y') {
+        console.log(correct + " Thank you for playing this guessing game, " + userName + "!");
+    } else if (response == 'no' || response == 'n') {
+        console.log(incorrect + " Thank you for playing this guessing game, " + userName + "!");
+    } else {
+        console.log(invalid + " Thank you for playing this guessing game, " + userName + "!");
+    }
 } else {
     console.log(invalid + " Thank you for playing this guessing game, " + userName + "!");
 }
