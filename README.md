ef078f0855f6a81911f6957566d5a3bbfc19f436
Chloe Nott <chloe@nott.email>
2021-09-15 21:00:11 -0700

    Adding changes for lab 3.

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # 201-lab
-For lab 02 and any additional that may arise
+
+## Lab 2:
+The purpose of the first lab was to create an about me webpage that contained a short bio about myself. There was also to be javascript added to create an about me 5 question (plus one) guessing game to demonstrate basic understanding of if/else statements, prompts, alerts, variables, and console logs. Functions, loops, and data strctures, were deliberately not used for this assignment.
+
+## Lab 3:
+Features to add:
+
+- ~~Implement the following features using only what has been taught in class so far. No functions. No helper methods.~~
+- ~~Change work experience and education parts of the bio to an unordered list.~~
+- ~Add a top ten list about myself using an ordered list.~
+- ~~For the guessing game, provide feedback on the responses.~~
+- ~~Add a 6th question that has a numeric answer. Give user feedback on high/low guesses. Allow 4 guesses. If guesses are used up, show the correct answer.~~
+- ~~Add a 7th question that has multiple answers. Store the correct answers in an array. Allow 6 guesses. Display all correct answers at the end. The game ends when user correctly guesses one of the possible answers.~~
+- ~~Keep score for the guessing game. Show user number of correct answers of the total questions asked (7).~~
+- ~~Use console logs to keep track of what questions have been asked.~~
+- ~~Add at least one more CSS style.~~
+- ~~Optional: put questions, answers, responses for first 5 questions into arrays (or array of arrays). Use for loop utilizing it.~~
+- ~~Optional: Make the 6th question a random number to guess.~~
+
+
+
+
+
+
--- script.js
+++ script.js
@@ -1,102 +1,203 @@
+`use strict`
+
 // What is your name?
-let userName = prompt("What is your name?")
-alert("Welcome " + userName + "!")
-
-// Generic alerts to yes/no answer questions.
-let invalid = "Invalid response.";
-let correct = "You were correct!"
-let incorrect = "That is incorrect. It's probably the other answer."
-
-// Deliberating not using a function, loop, nor map for the intent of this assignment.
-// Asks user a question. User is alerted whether answer was correct.
-// Invalid responses: not yes/y/no/n. Capitalization has no affect.
-let response = prompt('Are cats my favorite kind of pet?')
-if (typeof(response) == 'string') {
-    response = response.toLowerCase();
-    if (response == 'yes' || response == 'y') {
-        //console.log(correct);
-        alert(correct);
-    } else if (response == 'no' || response == 'n') {
-        //console.log(incorrect);
-        alert(incorrect);
+let userName = prompt("What is your name?");
+if (typeof(userName) == 'string') {
+    if (userName.length > 0) {
+        alert("Welcome " + userName + "!");
     } else {
-        //console.log(invalid);
-        alert(invalid);
+        alert("Welcome stranger!");
     }
 } else {
-    //console.log(invalid);
-    alert(invalid);
+    alert("Welcome stranger!");
 }
 
-response = prompt('Is watermelon not my favorite food?')
-if (typeof(response) == 'string') {
-    response = response.toLowerCase();
-    if (response == 'yes' || response == 'y') {
-        //console.log(incorrect);
-        alert(incorrect);
-    } else if (response == 'no' || response == 'n') {
-        //console.log(correct);
-        alert(correct);
+// Keep track of the number of correct guesses and the number of questions asked.
+// Be sure to increment questionsAsked if any additional questions are added later on.
+let correctCount = 0;
+let questionsAsked = 0;
+
+// Asks user the yes/no quesions. If correct, user is alerted then next question is asked.
+// Different alerts are shown depending on correct, incorrect, or invalid answers. 
+// Valid responses are yes/y and no/n. Not case sensitive. 
+// User can reattempt up to the guess limit.
+// User is not penalized for invalid responses.
+let questions = ['Are cats my favorite kind of pet?','Is watermelon not my favorite food?','Is my favorite color blue?','Do I enjoy listening to music?','Is Gundam Unicorn my favorite anime?'];
+let answers = ['yes','no','no','yes','yes'];
+
+// Generic messages for invalid input to yes/no answer questions:
+let requiresYesOrNo = "Invalid guess. Has to be yes/y or no/n answer.";
+let requiresResponse = "Guess cannot be left blank."
+
+// Variables for yes/no question loop.
+let alertResponse;
+let response;
+let validResponses = ['yes', 'y', 'no', 'n']
+
+// Question 1-5. Loop through the yes/no questions.
+for (let i = 0; i < questions.length; i++) {
+    questionsAsked++;
+
+    console.log(questions[i]);
+    response = prompt(questions[i]);
+
+    // User entered a response.
+    if (response) {
+        response = response.toLowerCase();
+
+        // Correct guess
+        if (response == answers[i] || response == answers[i][0]) {
+            correctCount++;
+            alertResponse = `Question ${i+1}: You were correct!`;
+            console.log(alertResponse);
+            alert(alertResponse);
+            continue; // Go to next question when guessed correctly.
+
+        // Incorrect guess
+        } else if (
+                response == validResponses[0] || 
+                response == validResponses[1] ||
+                response == validResponses[2] ||
+                response == validResponses[3]
+                ) {
+            alertResponse = `Question ${i+1}: That is incorrect.`;
+        
+        // Invalid guess
+        } else {
+            alertResponse = requiresYesOrNo;
+            i--; // Decrement index to so the question is asked again. (Workaround...)
+            questionsAsked--; // (Workaround...)
+        }
+
+    // Guess cannot be blank
     } else {
-        //console.log(invalid);
-        alert(invalid);
+        alertResponse = requiresResponse;
+        i--; // Decrement index to so the question is asked again. (Workaround...)
+        questionsAsked--; // (Workaround...)
     }
-} else {
-    //console.log(invalid);
-    alert(invalid)
+
+    console.log(alertResponse);
+    alert(alertResponse);
+
 }
 
-response = prompt('Is my favorite color blue?')
-if (typeof(response) == 'string') {
-    response = response.toLowerCase();
-    if (response == 'yes' || response == 'y') {
-        //console.log(incorrect);
-        alert(incorrect);
-    } else if (response == 'no' || response == 'n') {
-        //console.log(correct);
-        alert(correct);
-    } else {
-        //console.log(invalid);
-        alert(invalid);
+// Question 6: guess the random number 1-10.
+let randomNum = Math.floor(Math.random() * 10) + 1;
+let question = 'Guess a number between 1 and 10.';
+let requires1To10 = `Requires a numeric response between 1-10.`;
+let guessCount = 1;
+let guessLimit = 4;
+questionsAsked++;
+
+// Question 6. Allow multiple attempts up to the guessLimit.
+while (guessCount <= guessLimit) {
+    console.log(question);
+    response = Number(prompt(question));
+    
+    // Correct guess
+    if (response == randomNum) {
+        correctCount++;
+        alertResponse = `That is correct! You had ${guessLimit - guessCount} guesses remaining.`;
+        console.log(alertResponse);
+        alert(alertResponse);
+        break;
+    
+    // Valid guess but guess was too low
+    } else if (response >= 1 && response < randomNum) {
+        if (guessCount == guessLimit) {
+            alertResponse = `Guess was incorrect. No more guesses remaining. Answer was ${randomNum}.`
+        } else {
+            alertResponse = `Guess was too low. Guesses remaining: ${guessLimit - guessCount}`;
+        }
+        guessCount++; // Only want to penalize valid responses.
     }
-} else {
-    //console.log(invalid);
-    alert(invalid)
+
+    // Valid guess but guess was too high
+    else if (response <= 10 && response > randomNum) {
+        if (guessCount == guessLimit) {
+            alertResponse = `Guess was incorrect. No more guesses remaining. Answer was ${randomNum}.`
+        } else {
+            alertResponse = `Guess was too high. Guesses remaining: ${guessLimit - guessCount}`;
+        }
+        guessCount++; // Only want to penalize valid responses.
+    }
+
+    // Invalid guess
+    else {
+        alertResponse = requires1To10; // Do not penalize invalid responses.
+    }
+
+    // Code only reaches here if correct guess wasn't given.
+    console.log(alertResponse);
+    alert(alertResponse);
 }
 
-response = prompt('Do I enjoy listening to music?')
-if (typeof(response) == 'string') {
-    response = response.toLowerCase();
-    if (response == 'yes' || response == 'y') {
-        //console.log(correct);
-        alert(correct);
-    } else if (response == 'no' || response == 'n') {
-        //console.log(incorrect);
-        alert(incorrect);
+
+guessCount = 1;
+guessLimit = 6;
+questionsAsked++;
+question = 'What is one of my favorite movies/anime/series?'
+answers = [
+    'Gundam Unicorn',
+    'Mob Psycho 100',
+    'The Fountain',
+    'Spirited Away',
+    'Promare',
+    'Batman Trilogy',
+    'Star Trek TNG',
+    'Star Trek TOS',
+    'The Mandalorian',
+    'Wonder Woman (2017)'
+    ]
+
+question7:
+while (guessCount <= guessLimit) {
+    console.log(question);
+    response = prompt(question);
+
+    // User entered a response
+    if (response) {
+        response = response.toLowerCase();
+
+        // Check if guess is one of the possible answers.
+        for (let i = 0; i < answers.length; i++) {
+
+            // Correct guess was found in array of possible answers.
+            if (response == answers[i].toLowerCase()) {
+                correctCount++;
+                alertResponse = `${answers[i]} is one of my favorites! You had ${guessLimit - guessCount} guesses remaining. Possible answers were: ${answers}`;
+                console.log(alertResponse);
+                alert(alertResponse);
+                break question7; // No need to check for other correct guesses in array.
+            }
+        }
+
+        // Code only reaches here if guess was not found in array. Guess was not one of the possible answers.
+        if (guessCount == guessLimit) {
+            alertResponse = `Sorry! You have run out of attempts. Possible answers were: ${answers}`
+        } else {
+            alertResponse = `Nope! Guess again! Guesses left: ${guessLimit - guessCount}`;
+        }
+        guessCount++;
+
+    // Invalid guess
     } else {
-        //console.log(invalid);
-        alert(invalid);
+        alertResponse = requiresResponse; // Do not penalize invalid responses.
     }
-} else {
-    //console.log(invalid);
-    alert(invalid);
+
+    // Code only reaches here if correct guess wasn't given.
+    console.log(alertResponse);
+    alert(alertResponse);
 }
 
-
-response = prompt('Is Gundam Unicorn my favorite anime?')
-if (typeof(response) == 'string') {
-    response = response.toLowerCase();
-    if (response == 'yes' || response == 'y') {
-        //console.log(correct + " Thank you for playing this guessing game, " + userName + "!");
-        alert(correct + " Thank you for playing this guessing game, " + userName + "!");
-    } else if (response == 'no' || response == 'n') {
-        //console.log(incorrect + " Thank you for playing this guessing game, " + userName + "!");
-        alert(incorrect + " Thank you for playing this guessing game, " + userName + "!");
+// Alert final message.
+console.log(`You got ${correctCount} of ${questionsAsked} correct!`);
+if (typeof(userName) == 'string') {
+    if (userName.length > 0) {
+        alert(`You got ${correctCount} of ${questionsAsked} correct! Thank you for playing this guessing game, ${userName}!`);
     } else {
-        //console.log(invalid + " Thank you for playing this guessing game, " + userName + "!");
-        alert(invalid + " Thank you for playing this guessing game, " + userName + "!");
+        alert(`You got ${correctCount} of ${questionsAsked} correct! Thank you for playing this guessing game`);
     }
 } else {
-    //console.log(invalid + " Thank you for playing this guessing game, " + userName + "!");
-    alert(invalid + " Thank you for playing this guessing game, " + userName + "!");
+    alert(`You got ${correctCount} of ${questionsAsked} correct! Thank you for playing this guessing game`);
 }
